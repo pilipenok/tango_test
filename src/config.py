a2af547f09cc7acf83b814bce0f4ec9ac1673f9e
Oleg Pilipenok <pilipenok@gmail.com>
2021-09-09 13:01:01 +0400

    redis config for gcloud

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ app_name = 'tango'
 DEBUG = os.environ.get('DEBUG', False)
 PROFILE = os.environ.get('PROFILE', False)
 
-redis_host = "localhost"
+redis_host = '10.239.26.211'
 redis_port = 6379
 
 predictions_endpoint = 'http://127.0.0.1:5000/prediction'
