df3562ef7a7c030657be7b006ac6947f2750d93f
Oleg Pilipenok <pilipenok@gmail.com>
2021-09-09 18:11:03 +0400

    update redis config

--- cicd/terraform/network.tf
+++ cicd/terraform/network.tf
@@ -7,5 +7,5 @@ resource "google_vpc_access_connector" "vpc_connector" {
 
   network = google_compute_network.vpc_network.name
   region = var.region
-  ip_cidr_range = "10.2.0.0/28"
+  ip_cidr_range = "10.8.0.0/28"
 }
--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ gcp_bigtable_instance_id = "tango-instance"
 DEBUG = os.environ.get('DEBUG', False)
 PROFILE = os.environ.get('PROFILE', False)
 
-redis_host = '10.239.26.211'
-redis_port = 6379
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_port = os.environ.get('REDIS_PORT', 6379)
 
 predictions_endpoint = 'http://127.0.0.1:5000/prediction'
