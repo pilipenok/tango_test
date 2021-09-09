f1d1b6b1144c2733b6736193777eeec811c02a3c
Oleg Pilipenok <pilipenok@gmail.com>
2021-09-09 16:09:58 +0400

    redis network

--- cicd/terraform/redis.tf
+++ cicd/terraform/redis.tf
@@ -1,4 +1,7 @@
 resource "google_redis_instance" "redis" {
   name           = "redis"
   memory_size_gb = 1
+
+  location_id = var.zone
+  authorized_network = google_compute_network.vpc_network.name
 }
